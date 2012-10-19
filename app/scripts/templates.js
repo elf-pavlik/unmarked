3974f92a633bc99bc8a1241fb8585c76d4b28cc0
Sebastian Kippe <sebastian@kip.pe>
2012-10-19 18:50:47 +0100

    Add description

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -25,8 +25,11 @@ function program1(depth0,data) {
   stack1 = helpers.bindAttr.call(depth0, {hash:stack1,contexts:[],data:data});
   data.buffer.push(escapeExpression(stack1) + ">");
   stack1 = helpers._triageMustache.call(depth0, "url", {hash:{},contexts:[depth0],data:data});
-  data.buffer.push(escapeExpression(stack1) + "</a></p>\n    <p class=\"meta\">\n      ");
-  stack1 = helpers['if'].call(depth0, "tags", {hash:{},inverse:self.noop,fn:self.program(6, program6, data),contexts:[depth0],data:data});
+  data.buffer.push(escapeExpression(stack1) + "</a></p>\n    ");
+  stack1 = helpers['if'].call(depth0, "description", {hash:{},inverse:self.noop,fn:self.program(6, program6, data),contexts:[depth0],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    <p class=\"meta\">\n      ");
+  stack1 = helpers['if'].call(depth0, "tags", {hash:{},inverse:self.noop,fn:self.program(8, program8, data),contexts:[depth0],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n      <span class=\"created-at\">");
   stack1 = helpers._triageMustache.call(depth0, "createdAtTimeAgo", {hash:{},contexts:[depth0],data:data});
@@ -55,6 +58,14 @@ function program4(depth0,data) {
   return buffer;}
 
 function program6(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\n    <p class=\"description\">");
+  stack1 = helpers._triageMustache.call(depth0, "description", {hash:{},contexts:[depth0],data:data});
+  data.buffer.push(escapeExpression(stack1) + "</p>\n    ");
+  return buffer;}
+
+function program8(depth0,data) {
   
   var buffer = '', stack1;
   data.buffer.push("\n        <span class=\"tags\">");
